8beeb16ffcf77b80e2a50202ac1820804080e628
Dolmachi <ernestkuznecov8@gmail.com>
2025-04-20 02:20:14 +0300

    create consumer

---
+++ src/consumer.py
@@ -0,0 +1,29 @@
+import json
+from kafka import KafkaConsumer
+from database import MongoDBConnector
+from logger import Logger
+
+class Consumer:
+    def __init__(self):
+        self.logger = Logger(True).get_logger(__name__)
+        self.db = MongoDBConnector().get_database()
+        self.consumer = KafkaConsumer(
+            "predictions",
+            bootstrap_servers="kafka:9092",
+            group_id="prediction-group",
+            auto_offset_reset="earliest",
+            value_deserializer=lambda m: json.loads(m.decode("utf-8"))
+        )
+
+    def run(self):
+        for msg in self.consumer:
+            try:
+                self.db.predictions.insert_one(msg.value)
+                self.logger.info(f"Saved to MongoDB: {msg.value}")
+            except Exception:
+                self.logger.error("Error writing to MongoDB", exc_info=True)
+
+
+if __name__ == "__main__":
+    consumer = Consumer()
+    consumer.run()
